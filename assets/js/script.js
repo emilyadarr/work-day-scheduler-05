755d4447646bc1ab2d4a850bc7c30896d58c598d
Emily <emilyadarr@gmail.com>
2021-12-31 13:43:15 -0500

    save and load tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,59 +2,148 @@ var currentDay = moment().format("dddd, MMMM D YYYY");
 
 $("#currentDay").replaceWith(currentDay);
 
-var times = [
-  "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"
-];
+// var times = [
+//   "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"
+// ];
 
-var formEl = document.getElementById("form");
+// var formEl = document.getElementById("form");
 
 // create rows for time slots
-for (var i=0; i < times.length; i++) {
-  const rowEl = document.createElement("div");
-  rowEl.setAttribute("class", "row form-row")
-  const timeCol = document.createElement("div");
-  timeCol.setAttribute("class", "col-2 time");
-  timeCol.textContent = times[i];
-  const inputCol = document.createElement("textarea");
-  inputCol.setAttribute("class", "col input-text")
-  const formBtn = document.createElement("button");
-  formBtn.setAttribute("class", "btn saveBtn");
-  formBtn.innerHTML = "<span class='oi oi-hard-drive'></span>";
-  formEl.appendChild(rowEl);
-  rowEl.append(timeCol, inputCol, formBtn);
-}
+// for (var i=0; i < times.length; i++) {
+//   const rowEl = document.createElement("div");
+//   rowEl.setAttribute("class", "row form-row")
+//   const timeCol = document.createElement("div");
+//   timeCol.setAttribute("class", "col-2 time");
+//   timeCol.textContent = times[i];
+//   const inputCol = document.createElement("textarea");
+//   inputCol.setAttribute("class", "col input-text")
+//   const formBtn = document.createElement("button");
+//   formBtn.setAttribute("class", "btn saveBtn");
+//   formBtn.innerHTML = "<span class='oi oi-hard-drive'></span>";
+//   formEl.appendChild(rowEl);
+//   rowEl.append(timeCol, inputCol, formBtn);
+// }
 
 //CHECK THIS
 var tasks = {};
-// something wrong with save tasks - check tasks var
+
+var createTask = function(taskText, taskList) {
+  var taskNew = $("<textarea>")
+    .addClass("col input-text")
+    .text(taskText);
+
+  $("#row-" + taskList).append(taskNew);
+};
+
+var loadTasks = function() {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  // if nothing in localStorage, create a new array
+  if (!tasks) {
+    tasks = {
+      nineAM: "",
+      tenAM: "",
+      elevenAM: "",
+      twelvePM: "",
+      onePM: "",
+      twoPM: "",
+      threePM: "",
+      fourPM: "",
+      fivePM: ""
+    };
+  }
+
+  // for (var i = 0; i < tasks.length; i++) {
+  //   // var taskText = document.createElement("textarea");
+  //   // taskText.innerText = tasks[i];
+  //   // taskText.setAttribute("class", "col input-text");
+  //   $("#row-" + tasks[i]).append(tasks[i]);
+
+  //   };
+
+  $.each(tasks, function(list, currentValue) {
+    console.log(list, currentValue);
+   // currentValue.forEach(function(task) {
+      $("#row-" + list).append(currentValue);
+      //createTask(task.text, list);
+    // });
+  });
+};
+
+
+
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-$(".input-text").on("click", "textarea", function() {
-  var text = $(this)
-    .text()
-    .trim();
-  //replace textarea with new input
-  var textInput = $("<textarea>").addClass("col input-text").val(text);
-  $(this).replaceWith(textInput);
+// textarea was clicked
+// $(".input-text").on("click", "textarea", function() {
+//   var text = $(this)
+//     .text()
+//     .trim();
+//   //replace textarea with new input
+//   var textInput = $("<textarea>").addClass("col input-text").val(text);
+//   $(this).replaceWith(textInput);
 
-  saveTasks();
-});
+//   textInput.trigger("focus");
+//   //saveTasks();
+// });
+
+// $(".input-text").on("blur", "textarea", function() {
+//   var text = $(this).val();
+
+//   var status = $(this)
+//     .closest(".time-block")
+//     .attr("id")
+//     .replace("row-", "");
+//   var index = $(this)
+//     .closest(".input-text")
+//     .index();
+
+//   tasks[status][index].text = text;
+//   saveTasks();
+
+//   var taskUpdate = $("<textarea>")
+//     .addClass("col input-text")
+//     .text(text);
+
+//   $(this).replaceWith(taskUpdate);
+
+// });
 
 $(".saveBtn").click(function() {
-  var taskInput = $(".input-text").val();
+  var taskInput9 = document.getElementById("row-nineAM").value;
+  tasks.nineAM = taskInput9;
 
-  if (taskInput) {
-    tasks.push(taskInput)
-  };
+  var taskInput10 = document.getElementById("row-tenAM").value;
+  tasks.tenAM = taskInput10;
+  
+  var taskInput11 = document.getElementById("row-elevenAM").value;
+  tasks.elevenAM = taskInput11;
+  
+  var taskInput12 = document.getElementById("row-twelvePM").value;
+  tasks.twelvePM = taskInput12;
 
-  saveTasks();
+  var taskInput1 = document.getElementById("row-onePM").value;
+  tasks.onePM = taskInput1;
+
+  var taskInput2 = document.getElementById("row-twoPM").value;
+  tasks.twoPM = taskInput2;
+
+  var taskInput3 = document.getElementById("row-threePM").value;
+  tasks.threePM = taskInput3;
+
+  var taskInput4 = document.getElementById("row-fourPM").value;
+  tasks.fourPM = taskInput4;
+
+  var taskInput5 = document.getElementById("row-fivePM").value;
+  tasks.fivePM = taskInput5;
+
+
+ saveTasks();
 });
 
-var loadTasks = function() {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
-}
+
 
 loadTasks();
 
